1306fb7eaf4af78a095f24a6f2f0b90b57b2a066
AslanSN <aslan.staub@manumartinez.es>
2023-03-20 19:36:11 +0100

    initial state awakening implemented

--- src/features/users/Users.tsx
+++ src/features/users/Users.tsx
@@ -1,6 +1,19 @@
+//React
+import { useEffect } from 'react';
+//Redux
+import { useAppDispatch } from '../../app/hooks';
+//Functions
+import { fetchUsersAsync } from './slice/usersSlice';
+
 const Users: React.FC = () => {
+	const dispatch = useAppDispatch()
+
+	useEffect(() => {
+		dispatch(fetchUsersAsync())
+	})
 	return (
 		<>
+
 		</>
 )
 }
